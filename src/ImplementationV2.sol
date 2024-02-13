60b3bae0f2e2da476e6a47a1d591de66e8d1f761
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-02-14 01:56:55 +0530

    ImplementationV2 setup upgradeable

--- src/ImplementationV2.sol
+++ src/ImplementationV2.sol
@@ -1,15 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract ImplementationV2 {
-    uint256  internal number;
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract ImplementationV2 is
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable
+{
+    uint256 internal number;
+
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+    function initialize() public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
 
     function setNumber(uint256 _number) external {}
 
-    function getNumber() external view returns(uint256){
+    function getNumber() external view returns (uint256) {
         return number;
     }
-    function version() external pure returns(uint256) {
+    function version() external pure returns (uint256) {
         return 2;
     }
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
